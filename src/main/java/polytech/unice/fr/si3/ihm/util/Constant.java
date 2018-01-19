5c1d5261f4172f974d876f32d24d6fb7391f1789
Simon Serrano <simon.serrano@etu.unice.fr>
2018-01-19 15:57:24 +0100

    added add incident page not finished yet

--- src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
+++ src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
@@ -3,5 +3,6 @@ package polytech.unice.fr.si3.ihm.util;
 public class Constant {
 
     public static String MAIN_PAGE_FXML = "/fxml/main_view.fxml";
+    public static String ADD_INCIDENT_FXML = "/fxml/add_incident.fxml";
 
 }
