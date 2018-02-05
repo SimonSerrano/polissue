8d49a4011b6230173e61019edd41f61f9d028b02
Gregoire Peltier <idevedit@gmail.com>
2018-02-05 17:40:08 +0100

    Added the constant for the new fxml files

--- src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
+++ src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
@@ -1,8 +1,9 @@
 package polytech.unice.fr.si3.ihm.util;
 
 public class Constant {
-
     public static String MAIN_PAGE_FXML = "/fxml/main_view.fxml";
     public static String ADD_INCIDENT_FXML = "/fxml/add_incident.fxml";
+    public static String INCIDENT_CELL_FXML = "/fxml/incident_cell.fxml";
+    public static String INCIDENT_DETAILS_FXML = "/fxml/incident_details.fxml";
 
 }
