e1a481fd70072d01f327be77a0806e1466b00ed1
Gregoire Peltier <idevedit@gmail.com>
2018-02-06 20:21:36 +0100

    added the cell list to the MainViewController.java

--- src/main/java/polytech/unice/fr/si3/ihm/controller/MainViewController.java
+++ src/main/java/polytech/unice/fr/si3/ihm/controller/MainViewController.java
@@ -1,12 +1,24 @@
 package polytech.unice.fr.si3.ihm.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import polytech.unice.fr.si3.ihm.model.Incident;
+import polytech.unice.fr.si3.ihm.view.IncidentCell;
 
-public class MainViewController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -24,24 +36,42 @@ public class MainViewController{
     private JFXButton addIncidentButton;
 
     @FXML
+    private JFXListView<Incident> incidentsView;
+
+    @FXML
     void addIncident(MouseEvent event) {
-        if (event.equals(MouseEvent.MOUSE_RELEASED)){
-            Stage stage = new Stage();
+        if (event.equals(MouseEvent.MOUSE_RELEASED)) {
+            //open the new incident stage
+            //Stage incidentStage = new Stage();
         }
     }
 
     @FXML
     void searchIncident(MouseEvent event) {
-
+        throw new UnsupportedOperationException();
     }
 
 
-    public void initContent(){
-        searchButton.getStyleClass().add("button-raised");
-        addIncidentButton.getStyleClass().add("button-raised");
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<Incident> incidents = FXCollections.observableArrayList();
+        incidents.add(new Incident("Lampe Cassé", "ampoule qui a claqué", "Mosser"));
+        incidents.add(new Incident("Wifi Lent", "je peux pas dl GOT", "Collet"));
+
+
+        incidentsView.setCellFactory(
+                new Callback<ListView<Incident>, ListCell<Incident>>() {
+                    @Override
+                    public ListCell<Incident> call(ListView<Incident> param) {
+                        return new IncidentCell();
+                    }
+                }
+        );
+        incidentsView.setItems(incidents);
+
+
     }
-
-
 
 
 }
