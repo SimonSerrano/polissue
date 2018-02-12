59c987d076266aaaa50d6f66d876b845370d60e8
Gregoire Peltier <idevedit@gmail.com>
2018-02-12 17:45:49 +0100

    Corrected the JsonWriterTest.java for the new Jsonwriter behavior

--- src/test/java/polytech/unice/fr/si3/ihm/util/JsonWriterTest.java
+++ src/test/java/polytech/unice/fr/si3/ihm/util/JsonWriterTest.java
@@ -1,5 +1,6 @@
 package polytech.unice.fr.si3.ihm.util;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,11 +8,12 @@ import org.junit.jupiter.api.Test;
 import polytech.unice.fr.si3.ihm.factory.IncidentJSONFactory;
 import polytech.unice.fr.si3.ihm.model.Incident;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonWriterTest {
 
@@ -21,8 +23,18 @@ class JsonWriterTest {
 
     @BeforeEach
     void setUp() {
+        try (FileWriter fileWriter = new FileWriter(filePath)) {
+            fileWriter.write("[]\n");
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         writer = new JsonWriter();
+
     }
+
 
     @AfterEach
     void tearDown() throws IOException {
@@ -32,15 +44,23 @@ class JsonWriterTest {
         fileWriter.close();
     }
 
+    /**
+     * Try and write a test object to the incident list
+     *
+     * @throws IOException
+     */
     @Test
     void write() throws IOException {
+
+
         JSONObject expected = new JSONObject();
+
         expected.put("test", "test");
         writer.write(expected, filePath);
 
         String content = new String(Files.readAllBytes(Paths.get(filePath)));
-        JSONObject jsonObject = new JSONObject(content);
-        assertEquals(expected.toString(), jsonObject.toString());
+        JSONArray jsonObject = new JSONArray(content);
+        assertEquals(expected.toString(), jsonObject.get(0).toString());
 
     }
 
@@ -54,9 +74,9 @@ class JsonWriterTest {
         expected.put("title", "title");
         expected.put("description", "description");
         expected.put("declarer", "declarer");
-        JSONObject jsonObject = new JSONObject(content);
+        JSONArray jsonObject = new JSONArray(content);
 
-        assertEquals(expected.toString(), jsonObject.toString());
+        assertEquals(expected.toString(), jsonObject.get(0).toString());
 
 
     }
