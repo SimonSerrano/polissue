25c9dd3ae75b85b8ba2df2f1daeee673e438818b
Simon Serrano <simon.serrano@etu.unice.fr>
2018-03-10 16:55:33 +0100

    Removed unused pictures

--- src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
+++ src/main/java/polytech/unice/fr/si3/ihm/util/Constant.java
@@ -12,13 +12,7 @@ public class Constant {
 
 
     //Images
-    public static final String HEALTH_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-health-category.png";
-    public static final String BEHAVIOR_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-behavior-category.png";
-    public static final String LOGISTIC_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-logistic-category.png";
-    public static final String STUFF_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-stuff-category.png";
-    public static final String METEO_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-five-category.png";
-    public static final String OTHER_CATEGORY = System.getProperty("user.dir")+"/src/main/resources/img/polissue-other-category.png";
-    public static final String POLISSUE_BANNER = System.getProperty("user.dir")+"/src/main/resources/img/polissue-banner.png";
+    public static final String POLISSUE_BANNER = "file:src/main/resources/img/polissue-banner.png";
     public static final String POLISSUE_LOGO = "file:src/main/resources/img/polissue-logo.png";
 
     private Constant() {
