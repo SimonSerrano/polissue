6e96e43d0a125610b8f7f5ebdc2594e617895d0a
Simon Serrano <simon.serrano@etu.unice.fr>
2018-03-11 19:47:56 +0100

    fixed a bug with the css

--- src/main/java/polytech/unice/fr/si3/ihm/model/Category.java
+++ src/main/java/polytech/unice/fr/si3/ihm/model/Category.java
@@ -2,29 +2,24 @@ package polytech.unice.fr.si3.ihm.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import polytech.unice.fr.si3.ihm.util.Constant;
+
 
 public enum Category {
     //CATEGORY permet d'afficher cet enum par défaut dans la combobox lorsqu'aucune autre catégprie n est selectionnée
-    CATEGORY(null,"Catégorie"),
-    BEHAVIOR(Constant.BEHAVIOR_CATEGORY, "COMPORTEMENT"),
-    HEALTH(Constant.HEALTH_CATEGORY, "SANTÉ"),
-    LOGISTIC(Constant.LOGISTIC_CATEGORY, "LOGISTIQUE"),
-    STUFF(Constant.STUFF_CATEGORY, "MATÉRIEL"),
-    METEO(Constant.METEO_CATEGORY, "METEO"),
-    OTHER(Constant.OTHER_CATEGORY, "AUTRE");
+    CATEGORY("Catégorie"),
+    BEHAVIOR("COMPORTEMENT"),
+    HEALTH("SANTÉ"),
+    LOGISTIC("LOGISTIQUE"),
+    STUFF("MATÉRIEL"),
+    METEO("METEO"),
+    OTHER("AUTRE");
 
-    private String categoryFilePath;
     private StringProperty frenchString;
 
-    Category(String categoryFilePath, String frenchString) {
-        this.categoryFilePath = categoryFilePath;
+    Category(String frenchString) {
         this.frenchString = new SimpleStringProperty(frenchString);
     }
 
-    public String getCategoryFilePath() {
-        return categoryFilePath;
-    }
 
     public StringProperty getFrenchString() {
         return frenchString;
